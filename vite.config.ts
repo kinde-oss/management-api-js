52bb7b9066e7c5e4800b852b4eed6c3b1045dfb7
Daniel Rivers <me@danielrivers.com>
2024-04-12 00:18:23 +0100

    fixup: again

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,17 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 import dts from "vite-plugin-dts";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   build: {
     copyPublicDir: false,
     lib: {
       entry: resolve(__dirname, "lib/main.ts"),
       formats: ["es", "cjs"],
-      name: "@kinde/management-api-js",
+      name: "KindeManagementApiJs",
       fileName: "kinde-management-api-js",
     },
     target: "esnext",
   },
-  resolve: { alias: { src: resolve("lib/") } },
+  resolve: { alias: { src: resolve(__dirname, "lib/") } },
   plugins: [dts({ insertTypesEntry: true })],
 });
